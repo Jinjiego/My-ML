a50b7ebcf54cd2bd3e69e299826135ce79b8a86c
jinjiego <1285343948@qq.com>
2017-02-22 14:50:58 +0800

    gjj commit at 2017/2/22

---
+++ My ML/Main.py
@@ -0,0 +1,28 @@
+﻿
+from CH03.DTreeTest  import *
+from Ch05.Logistic import *
+from Ch07.AdaBoost import *
+from Ch08.Regression import *
+from Ch09.CART import *
+from Ch11.Apriori import  *
+from Ch12.FP_growth import *
+def main():
+     rgess=Regression()
+     rgess.debug()
+
+     fp=FP_growth()
+     fp.debug()
+
+     apr=Apriori()
+     apr.debug()
+
+     dtree=DTreeTest()
+     #dtree.test()
+     ab=AdaBoost()
+     # ab.Invoker()
+     cart=CART()
+     cart.Invoker()
+
+if __name__=='__main__':
+       main()
+    
